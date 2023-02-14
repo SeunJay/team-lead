f5447afe081786ca442674d2a53d91009ac0deb8
seunjay <seunjay92@gmail.com>
2023-02-14 10:31:21 +0100

    change

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ function App() {
                 </AlertDescription>
               </Alert>
             )}
-            <Text>Team Lead dApp.</Text>
+            <Text>Team Lead dAppz</Text>
             <Text>{account}</Text>
             <Text>{myBalance}</Text>
             <Button onClick={walletConnection} disabled={account}>
